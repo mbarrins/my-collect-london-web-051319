baa434db710df13d198d5d61866ccfb94e4fb748
Maija Barrins <maija.barrins@gmail.com>
2019-04-26 15:43:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < array.length
+      new_array << yield array[i]
+      i = i + 1
+    end
+    new_array
+  else
+    "This block should not run!"
+  end
+end
 
